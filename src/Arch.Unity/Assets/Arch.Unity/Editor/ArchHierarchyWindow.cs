64a0c2ac24371884aa2d720c096f215c48833029
KonH <konhit@gmail.com>
2025-06-16 18:25:32 +0200

    Copilot solution for component filtering

--- src/Arch.Unity/Assets/Arch.Unity/Editor/ArchHierarchyWindow.cs
+++ src/Arch.Unity/Assets/Arch.Unity/Editor/ArchHierarchyWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
@@ -22,24 +23,48 @@ namespace Arch.Unity.Editor
         HierarchyTreeView treeView;
         TreeViewState treeViewState;
 
+        // Component filtering state
+        bool _filteringEnabled = false; // Explicitly disabled by default
+        List<Type> _availableComponentTypes = new List<Type>();
+        List<Type> _selectedComponentTypes = new List<Type>();
+        Vector2 _componentFilterScrollPos;
+        bool _needComponentRefresh = false;
+
         void OnEnable()
         {
             treeViewState = new TreeViewState();
             treeView = new HierarchyTreeView(treeViewState);
 
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+            EditorApplication.update += OnEditorUpdate;
         }
 
         void OnDisable()
         {
             treeView.Dispose();
             EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            EditorApplication.update -= OnEditorUpdate;
         }
 
         void OnPlayModeStateChanged(PlayModeStateChange playModeStateChange)
         {
             treeView.SetSelection(Array.Empty<int>());
             Repaint();
+        }
+
+        void OnEditorUpdate()
+        {
+            // Check if we need to refresh components
+            if (_needComponentRefresh && _filteringEnabled)
+            {
+                var worlds = World.Worlds.Where(x => x != null).ToDictionary(x => x.Id, x => x);
+                if (worlds.ContainsKey(selectedWorldId))
+                {
+                    RefreshComponentTypes(worlds[selectedWorldId]);
+                    _needComponentRefresh = false;
+                    Repaint();
+                }
+            }
         }
 
         void OnGUI()
@@ -65,6 +90,16 @@ namespace Arch.Unity.Editor
                     }
                 }
 
+                // Add filter toggle button
+                GUILayout.Space(5);
+                var prevFilterState = _filteringEnabled;
+                _filteringEnabled = GUILayout.Toggle(_filteringEnabled, "Filter", EditorStyles.toolbarButton);
+                if (_filteringEnabled != prevFilterState && _filteringEnabled && worlds.ContainsKey(selectedWorldId))
+                {
+                    // When enabling filter, refresh component types
+                    RefreshComponentTypes(worlds[selectedWorldId]);
+                }
+
                 GUILayout.FlexibleSpace();
             }
 
@@ -74,9 +109,102 @@ namespace Arch.Unity.Editor
                 selectedWorldId = keys.First();
             }
 
-            treeView.SetWorld(worlds[selectedWorldId]);
+            var currentWorld = worlds[selectedWorldId];
+
+            // Draw component filter panel if filtering is enabled
+            if (_filteringEnabled)
+            {
+                DrawComponentFilterPanel();
+            }
+
+            // Pass filter settings to tree view
+            treeView.SetWorld(currentWorld);
+            treeView.SetComponentFilter(_filteringEnabled, _selectedComponentTypes);
+
             var treeViewRect = EditorGUILayout.GetControlRect(false, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
             treeView.OnGUI(treeViewRect);
+        }
+
+        void DrawComponentFilterPanel()
+        {
+            // Draw a box with all available component types as toggles
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("Filter by Components", EditorStyles.boldLabel);
+
+            if (_availableComponentTypes.Count == 0)
+            {
+                EditorGUILayout.LabelField("No components found in current world");
+            }
+            else
+            {
+                _componentFilterScrollPos = EditorGUILayout.BeginScrollView(_componentFilterScrollPos, GUILayout.MaxHeight(150));
+
+                foreach (var componentType in _availableComponentTypes)
+                {
+                    // Clean up the component type name by removing [] suffix
+                    var typeName = componentType.Name;
+                    if (typeName.EndsWith("[]"))
+                    {
+                        typeName = typeName.Substring(0, typeName.Length - 2);
+                    }
+                    var displayName = ObjectNames.NicifyVariableName(typeName);
+
+                    var isSelected = _selectedComponentTypes.Contains(componentType);
+                    var newIsSelected = EditorGUILayout.ToggleLeft(displayName, isSelected);
+
+                    if (newIsSelected != isSelected)
+                    {
+                        if (newIsSelected)
+                        {
+                            _selectedComponentTypes.Add(componentType);
+                        }
+                        else
+                        {
+                            _selectedComponentTypes.Remove(componentType);
+                        }
+                    }
+                }
+
+                EditorGUILayout.EndScrollView();
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+
+        void RefreshComponentTypes(World world)
+        {
+            _availableComponentTypes.Clear();
+            var uniqueComponentTypes = new HashSet<Type>();
+
+            // Get all entities in the world and check their components
+            foreach (var chunk in world.Query(new QueryDescription()))
+            {
+                for (int i = 0; i < chunk.Entities.Length; i++)
+                {
+                    var entity = chunk.Entities[i];
+                    if (!world.IsAlive(entity)) {
+                        continue;
+                    }
+
+                    // Get components for this specific entity
+                    var components = world.GetAllComponents(entity);
+                    foreach (var component in components)
+                    {
+                        if (component != null && !uniqueComponentTypes.Contains(component.GetType()))
+                        {
+                            uniqueComponentTypes.Add(component.GetType());
+                        }
+                    }
+                }
+            }
+
+            _availableComponentTypes.AddRange(uniqueComponentTypes.OrderBy(t => t.Name));
+
+            // If we still didn't find any components, set a flag to try again later
+            if (_availableComponentTypes.Count == 0)
+            {
+                _needComponentRefresh = true;
+            }
         }
     }
 }
--- src/Arch.Unity/Assets/Arch.Unity/Editor/HierarchyTreeView.cs
+++ src/Arch.Unity/Assets/Arch.Unity/Editor/HierarchyTreeView.cs
@@ -34,19 +34,34 @@ namespace Arch.Unity.Editor
             }
 
             public ItemType itemType;
-            
+
             public Entity entityReference;
         }
+
+        // Component filtering state
+        bool _filteringEnabled;
+        List<Type> _selectedComponentTypes = new List<Type>();
 
         public HierarchyTreeView(TreeViewState state) : base(state)
         {
 
         }
 
+        public void SetComponentFilter(bool enabled, List<Type> selectedComponentTypes)
+        {
+            _filteringEnabled = enabled;
+            _selectedComponentTypes.Clear();
+            if (selectedComponentTypes != null && selectedComponentTypes.Count > 0)
+            {
+                _selectedComponentTypes.AddRange(selectedComponentTypes);
+            }
+            Reload();
+        }
+
         public void SetWorld(World world)
         {
             var changed = TargetWorld != world;
-            
+
             TargetWorld = world;
             Reload();
 
@@ -86,32 +101,79 @@ namespace Arch.Unity.Editor
             {
                 for (int i = 0; i < chunk.Entities.Length; i++)
                 {
-	                if (TargetWorld.IsAlive(chunk.Entities[i]))
-	                {
-		                hierarchyRoot.AddChild(CreateItem(chunk.Entities[i]));
-	                }
+                    if (TargetWorld.IsAlive(chunk.Entities[i]))
+                    {
+                        var entity = chunk.Entities[i];
+
+                        // Apply filtering based on component types
+                        if (_filteringEnabled && _selectedComponentTypes.Count > 0)
+                        {
+                            if (!EntityMatchesFilter(entity))
+                            {
+                                continue;
+                            }
+                        }
+
+                        hierarchyRoot.AddChild(CreateItem(entity));
+                    }
                 }
             }
             return root;
         }
 
+        bool EntityMatchesFilter(Entity entity)
+        {
+            if (!_filteringEnabled || _selectedComponentTypes == null || _selectedComponentTypes.Count == 0)
+            {
+                return true;
+            }
+
+            // Get all components for this entity
+            var entityComponents = TargetWorld.GetAllComponents(entity);
+            if (entityComponents == null || entityComponents.Length == 0)
+            {
+                return false;
+            }
+
+            // Get component types from the entity
+            var entityComponentTypes = new HashSet<Type>();
+            foreach (var component in entityComponents)
+            {
+                if (component != null)
+                {
+                    entityComponentTypes.Add(component.GetType());
+                }
+            }
+
+            // Check if entity has all selected component types
+            foreach (var selectedType in _selectedComponentTypes)
+            {
+                if (!entityComponentTypes.Contains(selectedType))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         protected override void RowGUI(RowGUIArgs args)
         {
-            var item = (Item)args.item;
-            var disabled = TargetWorld.IsAlive(item.entityReference);
+        var item = (Item)args.item;
+        var disabled = TargetWorld.IsAlive(item.entityReference);
 
-            using (new EditorGUI.DisabledScope(disabled))
-            {
-                var iconImage = item.itemType == ItemType.World ? Styles.ModelImporterIcon.image : Styles.GameObjectIcon.image;
-                var iconRect = args.rowRect;
-                iconRect.x += GetContentIndent(args.item);
-                iconRect.width = 16f;
-                GUI.DrawTexture(iconRect, iconImage);
+        using (new EditorGUI.DisabledScope(disabled))
+        {
+            var iconImage = item.itemType == ItemType.World ? Styles.ModelImporterIcon.image : Styles.GameObjectIcon.image;
+            var iconRect = args.rowRect;
+            iconRect.x += GetContentIndent(args.item);
+            iconRect.width = 16f;
+            GUI.DrawTexture(iconRect, iconImage);
 
-                extraSpaceBeforeIconAndLabel = iconRect.width + 2f;
-                base.RowGUI(args);
-            }
+            extraSpaceBeforeIconAndLabel = iconRect.width + 2f;
+            base.RowGUI(args);
         }
+    }
 
         protected override void SelectionChanged(IList<int> selectedIds)
         {
